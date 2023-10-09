06b31872aced71b1a96bb9e26e3c66e2477447da
Klayton Pagel <147002610+KlaytonPagel@users.noreply.github.com>
2023-10-09 11:04:37 -0500

    Add files via upload

---
+++ StreakTracking.py
@@ -0,0 +1,18 @@
+import csv
+
+connections = {}
+
+# Open the CSV file and iterate through the rows adding it to the dictionary
+def get_dict():
+    with open('StreakCounter.csv', 'r') as file:
+        csvreader = csv.reader(file)
+        for row in csvreader:
+            item, term =row
+            connections[item] = term
+
+
+get_dict()
+
+# Display all names and date in a table for to the user
+for item in connections:
+    print(item, connections[item])
