af43db2e1f77c40a5f4ced0f2e8a6e5f1d96e011
Klayton Pagel <pagelk@lopers.unk.edu>
2023-10-24 15:33:29 -0500

    added get overdue connection
    added a function that compares current date to connection dates and returns what dates are overdue

--- StreakTracking.py
+++ StreakTracking.py
@@ -1,19 +1,44 @@
-import csv
-
-connections = {}
-
-
-# Open the CSV file and iterate through the rows adding it to the dictionary
-def get_dict():
-    with open('StreakCounter.csv', 'r') as file:
-        csvreader = csv.reader(file)
-        for row in csvreader:
-            item, term =row
-            connections[item] = term
+import pandas as pd
+import datetime
 
 
-get_dict()
+connections_file = 'StreakCounter.csv'
+last_run_file = 'last_run.txt'
 
-# Display all names and date in a table for to the user
-for item in connections:
-    print(item, connections[item])
+
+def set_last_run_date():
+    current_date = datetime.datetime.now()
+    with open(last_run_file, "w") as file:
+        file.write(current_date.strftime("%Y-%m-%d"))
+
+
+def read_last_run_date():
+    try:
+        with open(last_run_file, "r") as file:
+            last_run_date_str = file.read()
+            return datetime.datetime.strptime(last_run_date_str, "%Y-%m-%d")
+    except FileNotFoundError:
+        # If the file doesn't exist, return None
+        return None
+
+
+def get_sorted_data():
+    data = pd.read_csv(connections_file)
+    # Convert the 'Date' column to a datetime format
+    data['Date'] = pd.to_datetime(data['Date'])
+    # Sort the DataFrame by the 'Date' column in ascending order
+    data_sorted = data.sort_values(by='Date', ascending=True)
+    # Reset the index with drip = True
+    data_sorted = data_sorted.reset_index(drop=True)
+    # Print sorted data
+    return data_sorted
+
+
+def get_overdue_connections():
+    for date in get_sorted_data()['Date']:
+        if date <= read_last_run_date():
+            print(date)
+
+
+set_last_run_date()
+get_overdue_connections()
