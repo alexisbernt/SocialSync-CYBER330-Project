2b992e67aa1e3ccf0c5047518fe503be9dc8b78e
Klayton Pagel <pagelk@lopers.unk.edu>
2023-11-19 12:55:20 -0600

    Corrected Button Commands
    Button commands were set to calling the functions instead of passing the functions, corrected that by removing the parenthesis after the function

--- Connection_Manger_GUI/ConnectionManagerGUI.py
+++ Connection_Manger_GUI/ConnectionManagerGUI.py
@@ -1,6 +1,8 @@
 import tkinter
 from tkinter import Tk, Button
 from tkinter import ttk  # importing the ttk module for combobox (dropdown)
+
+
 class ConnectionManagerGUI:
 
     def __init__(self):
@@ -8,11 +10,11 @@ class ConnectionManagerGUI:
         self.upper_frame = tkinter.Frame(self.main_window, width=800, height=600)
         self.upper_frame2 = tkinter.Frame(self.main_window, width=800, height=600)
         self.body_frame = tkinter.Frame(self.main_window, width=800, height=600)
-        self.body_frame2 = tkinter.Frame(self.main_window, width=800, height = 600)
+        self.body_frame2 = tkinter.Frame(self.main_window, width=800, height=600)
         self.lower_frame = tkinter.Frame(self.main_window, width=800, height=600)
 
         # button for reveal
-        self.reveal_button = tkinter.Button(self.upper_frame, text='REVEAL', command=self.get_sorted_data())
+        self.reveal_button = tkinter.Button(self.upper_frame, text='REVEAL', command=self.get_sorted_data)
         self.reveal_button.pack()
 
         # dropdown display for selecting which connection to update
@@ -30,7 +32,7 @@ class ConnectionManagerGUI:
         self.update_name_entry.pack(side='left')
         self.update_date.pack(side='left')
         self.update_date_entry.pack(side='left')
-        self.update_button = tkinter.Button(self.upper_frame2, text='UPDATE', command=self.get_sorted_data())
+        self.update_button = tkinter.Button(self.upper_frame2, text='UPDATE', command=self.get_sorted_data)
         self.update_button.pack()
         self.spacing = tkinter.Label(self.upper_frame2, text=' ')
         self.spacing.pack()
@@ -40,7 +42,7 @@ class ConnectionManagerGUI:
         self.add_name = tkinter.Entry(self.body_frame, width=10)
         self.prompt_date = tkinter.Label(self.body_frame, text='Date to add: ')
         self.add_date = tkinter.Entry(self.body_frame, width=10)
-        self.add_button = tkinter.Button(self.body_frame, text='ADD', command=self.delete())
+        self.add_button = tkinter.Button(self.body_frame, text='ADD', command=self.delete)
         self.prompt_name.pack(side='left')
         self.add_name.pack(side='left')
         self.prompt_date.pack(side='left')
@@ -52,7 +54,7 @@ class ConnectionManagerGUI:
         self.remove_name = tkinter.Entry(self.body_frame2, width=10)
         self.prompt_removal_date = tkinter.Label(self.body_frame2, text='Date to remove: ')
         self.remove_date = tkinter.Entry(self.body_frame2, width=10)
-        self.remove_button = tkinter.Button(self.body_frame2, text='REMOVE', command=self.display_prompt())
+        self.remove_button = tkinter.Button(self.body_frame2, text='REMOVE', command=self.display_prompt)
         self.prompt_removal_name.pack(side='left')
         self.remove_name.pack(side='left')
         self.prompt_removal_date.pack(side='left')
